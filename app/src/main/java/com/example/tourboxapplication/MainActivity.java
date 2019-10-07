d7eb32c0a88c857e1a9fb9c9fc7dd37424f3f7f8
曹小黑 <1252106074@qq.com>
2019-10-07 15:02:46 +0800

    啊

---
+++ app/src/main/java/com/example/tourboxapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.tourboxapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button imb;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);    }
+
+
+
+}
